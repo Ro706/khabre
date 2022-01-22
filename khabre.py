d355e8ba7a5f555e0d323ad9b42803d20d406bd5
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2022-01-22 19:50:27 +0530

    Add files via upload

---
+++ khabre.py
@@ -0,0 +1,22 @@
+import os
+import requests
+from gtts import gTTS
+def speack(a):
+    language ='en'
+    output=gTTS(text=a,lang=language)
+    output.save('todays_news.mp3')
+    os.system('mpv /data/data/com.termux/files/home/code/todays_news.mp3')
+    os.system('rm -r /data/data/com.termux/files/home/code/todays_news.mp3')
+api_key='a8c42b827a044f0b90d1d1fb314ef182'
+def news():
+    main_url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey="+api_key
+    news=requests.get(main_url).json()
+    article=news["articles"]
+    news_article=[]
+    for arti in article:
+        news_article.append(arti["title"])
+    for i in range(len(news_article)):
+        a=i+1,news_article[i]
+        print (i+1,news_article[i])
+        speack(news_article[i])
+news()
